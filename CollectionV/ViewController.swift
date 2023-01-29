5e0b791a626c47de8214ba21f22126d3a4f92066
Pratik Gavit <pratikgavit008@gmail.com>
2023-01-29 11:25:13 +0530

    Initial Commit

---
+++ CollectionV/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  CollectionV
+//
+//  Created by Pratik Gavit on 29/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+}
+
+
+
+extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? UICollectionViewCell
+        else {fatalError()}
+        cell.backgroundColor = .cyan
+        return cell
+    }
+}
+
+
+extension ViewController:UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = collectionView.bounds.width/2 - 7.5
+        return CGSize(width:size , height: size)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5 //This is space in scroll direction
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2.5//Between two perpendicular to scroll direction
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+    }}
